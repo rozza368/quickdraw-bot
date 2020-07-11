e0eb4d9e9a1b9c477dba9973662afb6634f358e2
rozza368 <rorytheb3st@gmail.com>
2020-07-11 12:16:34 +0800

    Added basic bot functionality

--- bot.py
+++ bot.py
@@ -1,6 +1,41 @@
+# Use discord.py 1.3.3
 import discord
+from discord.ext import commands
+from discord import Game
 
-class main:
+import data
+from token import TOKEN
+
+
+class Bot:
+    bot = commands.Bot(
+            command_prefix = ".",
+            case_insensitive = True,
+            description = "Holds data for Quick Draw! players.",
+            self_bot = False
+        )
+    
+
     def __init__(self):
-        # Main
-        print('init')
+        pass
+    
+
+    @bot.command(name="hello")
+    async def hello(self, ctx):
+        await ctx.send("Hi!")
+
+
+    @bot.event
+    async def on_ready(self):
+        print("Successfully logged in")
+        activity = discord.Activity(name='Quick Draw!', type=discord.ActivityType.playing)
+        await self.bot.change_presence(activity=activity)
+    
+
+    def run(self, token):
+        self.bot.run(token)
+
+
+if __name__ == "__main__":
+    bot = Bot()
+    bot.run(TOKEN)
