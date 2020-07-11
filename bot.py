a0a87f49004221f87aea4291b9e88de711230299
ALTERA-LLC <samscott640@gmail.com>
2020-07-11 13:16:25 +0800

    fixed

--- bot.py
+++ bot.py
@@ -5,20 +5,22 @@ from discord import Game
 import data
 from bot_token import TOKEN
 
-
-
-
 bot = commands.Bot(
-        command_prefix = ".",
-        case_insensitive = True,
-        description = "Holds data for Quick Draw! players.",
-        self_bot = False
-    )
+    command_prefix=".",
+    case_insensitive=True,
+    description="Holds data for Quick Draw! players.",
+    self_bot=False
+)
 
 
 @bot.command(name="hello")
 async def hello(ctx):
     await ctx.send("Hi!")
+
+
+@bot.command(name="Stats")
+async def hello(ctx):
+    await ctx.send("Thats mot a feature silly")
 
 
 @bot.event
@@ -27,9 +29,7 @@ async def on_ready():
     print(f"ID: {bot.user.id}\nUsername: {bot.user.name}")
     activity = discord.Activity(name='Quick Draw!', type=discord.ActivityType.playing)
     await bot.change_presence(activity=activity)
-    
 
 
 if __name__ == "__main__":
     bot.run(TOKEN)
-
