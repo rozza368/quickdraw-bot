19bbbd3c69e4e2e354a9a279447f5144e8de6fb1
ALTERA-LLC <samscott640@gmail.com>
2020-07-11 12:10:20 +0800

    added Bot stuff

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import discord
-
+from bot.TOKEN import TOKEN
 
 class main:
     def __init__(self):
         # Main
         print('init')
+        self.client = discord.Client()  # starts the discord client
 
+MainMF = main()
+MainMF.client.run(TOKEN)
