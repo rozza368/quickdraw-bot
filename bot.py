d7c64a3039815840d74a2e335fa399a5140c5f7e
rozza368 <rorytheb3st@gmail.com>
2020-07-11 13:07:12 +0800

    Fixed leftovers from the old class

--- bot.py
+++ bot.py
@@ -17,16 +17,16 @@ bot = commands.Bot(
 
 
 @bot.command(name="hello")
-async def hello(self, ctx):
+async def hello(ctx):
     await ctx.send("Hi!")
 
 
 @bot.event
-async def on_ready(self):
+async def on_ready():
     print("Successfully logged in")
     print(f"ID: {bot.user.id}\nUsername: {bot.user.name}")
     activity = discord.Activity(name='Quick Draw!', type=discord.ActivityType.playing)
-    await self.bot.change_presence(activity=activity)
+    await bot.change_presence(activity=activity)
     
 
 
