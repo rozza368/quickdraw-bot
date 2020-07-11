fbf55abb1814db2db9c95738dab90d86302bed2c
ALTERA-LLC <samscott640@gmail.com>
2020-07-11 13:59:55 +0800

    idk

--- bot.py
+++ bot.py
@@ -5,22 +5,20 @@ from discord import Game
 import data
 from bot_token import TOKEN
 
+
+
+
 bot = commands.Bot(
-    command_prefix=".",
-    case_insensitive=True,
-    description="Holds data for Quick Draw! players.",
-    self_bot=False
-)
+        command_prefix = ".",
+        case_insensitive = True,
+        description = "Holds data for Quick Draw! players.",
+        self_bot = False
+    )
 
 
 @bot.command(name="hello")
 async def hello(ctx):
     await ctx.send("Hi!")
-
-
-@bot.command(name="Stats")
-async def hello(ctx):
-    await ctx.send("Thats mot a feature silly")
 
 
 @bot.event
@@ -33,3 +31,4 @@ async def on_ready():
 
 if __name__ == "__main__":
     bot.run(TOKEN)
+
