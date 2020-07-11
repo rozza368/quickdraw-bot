1a21186d451e5659e93836a5b282f4976d827c96
rozza368 <rorytheb3st@gmail.com>
2020-07-11 15:57:43 +0800

    Added several more functions to data.py

--- data.py
+++ data.py
@@ -24,6 +24,7 @@ class UserData:
     
 
     def get_inv(self, user):
+        self.check_usr(user)
         inv = self.data.get(user).get("inventory")
         if inv:
             return ", ".join(inv)
@@ -32,18 +33,37 @@ class UserData:
     
 
     def get_value(self, user, key):
+        self.check_usr(user)
         return self.data[user].get(key)
+    
+
+    def set_value(self, user, key, value):
+        self.check_usr(user)
+        self.data[user][key] = value
+    
+
+    def set_profile(self, user, key, value):
+        self.check_usr(user)
+        self.data[user]["profile"][key] = value
+    
+
+    def has_profile(self, user):
+        self.check_usr(user)
+        # true if the name is filled out
+        return self.data[user]["profile"]["name"]
     
 
     def is_usr(self, user):
         return bool(self.data.get(user))
 
 
-    def init_usr(self, user):
-        self.data[user] = self.default_data
-        self.save()
+    def check_usr(self, user):
+        if not self.is_usr(user):
+            self.data[user] = self.default_data
+            self.save()
 
 
 if __name__ == "__main__":
+    # testing
     ud = UserData()
     print(ud.is_usr("asdf"))
